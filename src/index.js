711125a10b6af6c44d3b1ce9c75855a73c685336
Martin Kruse <mkruse@prodyna.com>
2019-04-30 16:02:50 +0200

    PAC-40 implemented dummy lambda service

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const data = require("./data.json");
 
-exports.handler = (event, context, callback) => {
-  callback(null, data);
+exports.handler = async (event, context) => {
+  console.log(
+    "START pd-pac-conference-dal; event:",
+    JSON.stringify(event, null, 2),
+    "context: ",
+    JSON.stringify(context, null, 2)
+  );
+
+  return {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify(data)
+  };
 };
